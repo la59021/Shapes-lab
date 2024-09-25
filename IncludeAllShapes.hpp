646e16ae2cd03df7004d7767e578f24d1546d520
AJ Lehmann <la59021@mail.harpercollege.edu>
2024-09-25 16:20:17 -0500

    Added IncludeAllShapes.hpp

---
+++ IncludeAllShapes.hpp
@@ -0,0 +1,11 @@
+#ifndef INCLUDEALLSHAPES_HPP_INCLUDED
+#define INCLUDEALLSHAPES_HPP_INCLUDED
+
+#include "Circle.hpp"
+#include "Rectangle.hpp"
+#include "RightTriangle.hpp"
+//#include "Square.hpp"
+//#include "IsocelesRightTriangle"
+
+
+#endif // INCLUDEALLSHAPES_HPP_INCLUDED
