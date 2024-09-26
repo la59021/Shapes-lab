d6cf062cad7d8a13d8ca5e587d85d72e26261fc5
AJ Lehmann <la59021@mail.harpercollege.edu>
2024-09-26 15:52:02 -0500

    Finished square

--- Square.cpp
+++ Square.cpp
@@ -1,7 +1,8 @@
 #include "Square.hpp"
 
-Square::Square(double w) {
-    super()
+Square::Square(double w) : Rectangle(w, w) {
+    Rectangle(w, w);
+    //super()
     width = w;
     height = w;
 }
